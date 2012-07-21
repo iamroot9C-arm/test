2d688b0a533f699a7fd5e87b0f3b19922c510baf
Bon-gyu Koo <protocolstack9@gmail.com>
2012-07-22 04:11:23 +0900

    git test2

--- hello.c
+++ hello.c
@@ -1,10 +1,18 @@
+#if 0
 yes, hello.
+#endif
 #include <stdio.h>
+#include "sum.h"
 
 // 한글 테스트.. 
 int main(int argc, char* argv[])
 {
+    int a = 10;
+    int b = 20;
+
 	printf("Hello\n");
+    printf("sum: %d + %d = %d\n", a, b, sum(a,b));
+
 	return 0;
 }
 
