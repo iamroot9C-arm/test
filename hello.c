3cb21c1c983f7befe6469d8027b1ca13d466feb2
HongWoo Lee <hongwoo7@gmail.com>
2012-07-24 11:23:33 +0900

    - Hello World를 출력하도록 기능 변경.

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
     int a = 10;
     int b = 20;
 
-	printf("Hello\n");
+	printf("Hello World\n");
     printf("sum: %d + %d = %d\n", a, b, sum(a,b));
 
 	return 0;
